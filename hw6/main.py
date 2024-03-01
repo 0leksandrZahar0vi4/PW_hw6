d31f0aa30cf9287c938f2737edfe85eea0b5398d
OleksandrZahar0vi4 <zaharovich007@gmail.com>
2024-03-01 22:37:38 +0200

    fifth_commit

--- hw6/main.py
+++ hw6/main.py
@@ -27,13 +27,13 @@ for teacher_id in range(1, 4):
 # Додавання студентів і оцінок
 for group_id in range(1, 4):
     for _ in range(10):
-        cur.execute("INSERT INTO users (name, email, age, group_id) VALUES (%s, %s)",
-                    (fake.name(), fake.email(), random.randint(18,61), group_id))
-    users_id = cur.fetchone()[0]
-    for subjects_id in range(1, 3):
-        for _ in range(10):
-            cur.execute("INSERT INTO grades (user_id, subjects_id, grade, grade_date) VALUES (%s, %s, %s, %s)",
-                        (users_id, subjects_id, random.randint(60, 100), fake.date_this_decade()))
+        cur.execute("INSERT INTO users (name, email, age, group_id) VALUES (%s, %s, %s, %s) RETURNING id",
+                    (fake.name(), fake.email(), random.randint(18, 60), group_id))
+users_id = cur.fetchone()[0]
+for subjects_id in range(1, 3):
+    for _ in range(10):
+        cur.execute("INSERT INTO grades (user_id, subjects_id, grade, grade_date) VALUES (%s, %s, %s, %s)",
+                    (users_id, subjects_id, random.randint(60, 100), fake.date()))
 
 try:
     # Збереження змін
